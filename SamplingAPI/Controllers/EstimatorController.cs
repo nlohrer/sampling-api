cc7fa4eff6992bedbc7cbe0c2aae5068c4ae441b
apsa <n.Lohrer@campus.lmu.de>
2024-01-04 22:34:31 +0100

    Add initial scaffold

---
+++ SamplingAPI/Controllers/EstimatorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SamplingAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EstimatorController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<string>> HelloWorld()
+    {
+        return Ok("Hello, world!");
+    }
+}
---
+++ SamplingAPI/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "Sampling API",
+        Description = "An API to provide useful tools for sampling",
+        Version = "v1"
+    });
+    string xmlFilename = $"{Assembly.GetExecutingAssembly().Location}.xml";
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment() || app.Configuration.GetValue("USE_SWAGGER_UI", false))
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+public partial class Program { }
